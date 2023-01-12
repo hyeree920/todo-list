7671a358aa750274aa6b651ec185ca5ec0005644
hyeree920 <chlgofl336@naver.com>
2023-01-12 15:49:57 +0900

    1주차

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("test");
-print('puch test');
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from routers import view
+from routers.v1 import todo
+
+app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+app.include_router(view.router)
+app.include_router(todo.router)
